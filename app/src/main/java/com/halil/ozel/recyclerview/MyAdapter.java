669c4c22bc3ffffdc197dd691b44ec89103b13aa
Halil Özel <halilozel1903@gmail.com>
2018-07-26 15:45:58 +0300

    item ekleme ve silme islemleri yapıldı.

--- app/src/main/java/com/halil/ozel/recyclerview/MyAdapter.java
+++ app/src/main/java/com/halil/ozel/recyclerview/MyAdapter.java
@@ -14,9 +14,10 @@ import java.util.ArrayList;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
 
-     ArrayList<Manzara> mDataList;
-     LayoutInflater inflater;
+     ArrayList<Manzara> mDataList; // Manzara türünde bir ArrayList tanımı
+     LayoutInflater inflater; // LayoutInflater nesnesi tanımı
 
+     // 2 parametre alan bir constructor metodu tanımlandı.İnflate işlemi yap
     public MyAdapter(Context context, ArrayList<Manzara> data) {
 
        // inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -25,6 +26,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         this.mDataList = data;
     }
 
+    // inflate işlemi yapılan yapıyı ekleme işlemi yapılıyor.
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -43,9 +45,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     }
 
+    // item silme işlemleri
+    public void deleteItem(int position){
+        mDataList.remove(position); // elemanı listeden sil
+        notifyItemRemoved(position); // elemanı ekrandan sil
+        notifyItemRangeChanged(position,mDataList.size()); // silinen elemanla birlikte item değerlerini güncelle.
+
+    }
+
+
+
+    // item ekleme işlemleri
+    public void addItem(int position,Manzara kopyalanacakManzara){
+
+        mDataList.add(position,kopyalanacakManzara); // elemanı listeye ekle.
+        notifyItemInserted(position); // elemanı ekrana ekle
+        notifyItemRangeChanged(position,mDataList.size()); // eklenen elemanla birlikte item değerlerini güncelle.
+
+        // notifyDataSetChanged(); değişlikleri inceler ve tekrardan işlemleri alır. Animasyon bulunmaz.
+    }
+
     @Override
     public int getItemCount() {
-        return mDataList.size();
+        return mDataList.size(); // recyclerView'da ne kadar eleman var onu belirttik.
     }
 
 
@@ -54,24 +76,54 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
         TextView mManzaraBaslik,mManzaraAciklama;
         ImageView mManzaraResmi,mSilResmi,mKopyalaResmi;
+        int pozisyon = 0;
+        Manzara copyManzara;
 
+        // itemlarda ne tip işlemler yapılacak onu belirtiyoruz.
         public MyViewHolder(@NonNull View itemView) {
 
             super(itemView);
 
+            //elemanlar bulundu.
             mManzaraBaslik = itemView.findViewById(R.id.manzara_baslik);
             mManzaraAciklama = itemView.findViewById(R.id.manzara_aciklama);
             mManzaraResmi = itemView.findViewById(R.id.manzara_ekle);
             mSilResmi = itemView.findViewById(R.id.manzara_sil);
             mKopyalaResmi = itemView.findViewById(R.id.manzara_resim);
 
+            // butona tıklanınca item ekleme işlemi yap
+            mManzaraResmi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    addItem(pozisyon,copyManzara); // metod çağrılıp pozisyon ve manzara item değeri verildi.
+                }
+            });
+
+
+            // butona tıklanınca silme işlemini yap.
+            mSilResmi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    deleteItem(pozisyon); // metod çağrılıp pozisyon değeri verildi.
+                }
+            });
+
+
+
+
+
         }
 
+        // her bir cell'de bulunan verileri değiştirme metodu
         public void setData(Manzara tiklanilanResim, int i) {
 
+            // manzaraya ait resimlerin,açıklamaların,adların atandağı yerdir.
             this.mManzaraBaslik.setText(tiklanilanResim.getBaslik());
             this.mManzaraAciklama.setText(tiklanilanResim.getTanim());
             this.mKopyalaResmi.setImageResource(tiklanilanResim.getImageID());
+            this.pozisyon=i;
+            this.copyManzara = tiklanilanResim;
         }
     }
 
